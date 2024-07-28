c07c193f91dc6fe39d632219863eb8489f09251b
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-07-28 19:32:55 -0300

    fix: add https to url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_URL =
-  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ||
+  "https://" + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ||
   "http://localhost:3000";
 
 export const IPFS_URL =
