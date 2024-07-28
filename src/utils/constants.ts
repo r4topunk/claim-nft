1082e1575fc134a34e4bb618b873c15a9b5093fb
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-07-28 20:11:50 -0300

    fix: add node env control for project url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const PROJECT_URL =
-  "https://" + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ||
-  "http://localhost:3000";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://" + process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
 
 export const IPFS_URL =
   "https://harlequin-general-shrimp-966.mypinata.cloud/ipfs/";
